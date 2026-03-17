31c7c8a4f8cc8d35346d3c41e96baa14929bc866
Nikesh <nikeshnikesh520@gmail.com>
2026-03-17 06:49:20 +0530

    Prototype for todo-cotainer

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const res = await axios.get("http://localhost:5000/todos");
+    setTodos(res.data);
+  };
+
+  const addTodo = async () => {
+    if (!text) return;
+    await axios.post("http://localhost:5000/todos", { text });
+    setText("");
+    fetchTodos();
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Todo App</h1>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
